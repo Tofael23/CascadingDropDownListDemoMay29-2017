a3c0d5c3d0758077c07068a2e9d3ef3249047519
Tofael23 <51793134+Tofael23@users.noreply.github.com>
2020-01-28 02:29:06 +0600

    Created Date 27-05-2017

---
+++ CascadingDropDownListDemo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using CascadingDropDownListDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CascadingDropDownListDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCTutorialEntities db = new MVCTutorialEntities();
+        public ActionResult Index()
+        {
+            List<Country> CountryList = db.Countries.ToList();
+            ViewBag.CountryList = new SelectList(CountryList, "CountryId", "CountryName");
+            return View();
+        }
+
+        public JsonResult GetStateList(int CountryId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<State> StateList = db.States.Where(x => x.CountryId == CountryId).ToList();
+            return Json(StateList, JsonRequestBehavior.AllowGet);
+
+        }
+        
+    }
+}
---
+++ CascadingDropDownListDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CascadingDropDownListDemo.Startup))]
+namespace CascadingDropDownListDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
